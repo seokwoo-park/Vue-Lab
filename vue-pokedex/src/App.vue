89f1985e54fbf5dd59e053b9b201249b6ec32185
Seokwoo Park <dev.seokwoopark@gmail.com>
2022-03-03 14:26:24 +0100

    add CSS Reset

--- vue-pokedex/src/App.vue
+++ vue-pokedex/src/App.vue
@@ -2,4 +2,39 @@
 
 <template>Hello World</template>
 
-<style></style>
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
+  font-weight: normal;
+  margin: 0;
+  padding: 0;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+</style>
