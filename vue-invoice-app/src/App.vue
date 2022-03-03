0c2bc7bf287ede2b94e33e9c9490ea0eb5001561
Seokwoo Park <dev.seokwoopark@gmail.com>
2022-03-03 17:01:11 +0100

    [feature] Initialize Invocie-app tamplate

---
+++ vue-invoice-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>HELLO</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+</style>
+>
